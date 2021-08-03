21dd9211497a2a0de998022bacfb8cd678bd55e7
Diego Novais <contato@diegonovais.com.br>
2021-08-03 00:42:21 -0300

    Implement the game when player 02 is the winner

--- lib/game.rb
+++ lib/game.rb
@@ -27,6 +27,12 @@ class Game
       'Player 01 win!'
     elsif player_01 == STONE && player_02 == SCISSOR
       'Player 01 win!'
+    elsif player_02 == SCISSOR && player_01 == PAPER
+      'Player 02 win!'
+    elsif player_02 == PAPER && player_01 == STONE
+      'Player 02 win!'
+    elsif player_02 == STONE && player_01 == SCISSOR
+      'Player 02 win!'
     end
   end
 
--- test/game_test.rb
+++ test/game_test.rb
@@ -47,6 +47,47 @@ describe Game do
       end
     end
 
+    describe "when player 02 win" do
+      describe "when player 02 choose scissor and player 02 choose paper" do
+        it "should return player 02 win" do
+          player_01_choice = PAPER
+          player_02_choice = SCISSOR
+
+          game = Game.new(player_01_choice, player_02_choice)
+
+          match = game.play
+
+          expect(match).must_equal 'Player 02 win!'
+        end
+      end
+
+      describe "when player 02 choose paper and player 02 choose stone" do
+        it "should return player 02 win" do
+          player_01_choice = STONE
+          player_02_choice = PAPER
+
+          game = Game.new(player_01_choice, player_02_choice)
+
+          match = game.play
+
+          expect(match).must_equal 'Player 02 win!'
+        end
+      end
+
+      describe "when player 02 stone paper and player 02 choose scissor" do
+        it "should return player 02 win" do
+          player_01_choice = SCISSOR
+          player_02_choice = STONE
+
+          game = Game.new(player_01_choice, player_02_choice)
+
+          match = game.play
+
+          expect(match).must_equal 'Player 02 win!'
+        end
+      end
+    end
+
     describe "when players is draw" do
       describe "when player 01 and player 02 chooses scissors" do
         it "should return draw" do
